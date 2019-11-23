16f5f64688ccb5d3f66d4dfa5bb3f42a83c459e3
aliardan <33637478+aliardan@users.noreply.github.com>
2019-11-23 20:03:05 +0300

    Created project

---
+++ ImageProcessing5/Controllers/MirrorController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ImageProcessing5.Controllers
+{
+    public class MirrorController
+    {
+    }
+}
